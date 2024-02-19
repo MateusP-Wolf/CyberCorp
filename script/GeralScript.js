29771b49b688b472cdbe659dbb6dec7d5c650c4d
MateusP-Wolf <mateuspedriniwolf@gmail.com>
2024-02-19 13:53:16 -0300

    easter egg e correção do header
    .

--- script/GeralScript.js
+++ script/GeralScript.js
@@ -1,23 +1,25 @@
 // Quando a página é carregada, verifique o estado salvo no localStorage
 if (localStorage.getItem('menuAberto') === 'true') {
     abrir = 1;
-    document.getElementById("overlay").style.display = 'block';
+    document.getElementById("overlay").style.display = 'block'
   } else {
     abrir = 0;
-    document.getElementById("overlay").style.display = 'none';
+    document.getElementById("overlay").style.display = 'none'
   }
   
   function abrirMenu() {
-      var fundoOpaco = document.getElementById("overlay");
-      if (abrir === 0) {
-          abrir = 1;
-          fundoOpaco.style.display = 'block';
-          // Salve o estado no localStorage
-          localStorage.setItem('menuAberto', 'true');
-      } else {
+      var fundoOpaco = document.getElementById("overlay")
+      if (abrir === 1) {
           abrir = 0;
-          fundoOpaco.style.display = 'none';
+          console.log('aberto')
+          fundoOpaco.style.display = 'block'
           // Salve o estado no localStorage
-          localStorage.setItem('menuAberto', 'false');
+          localStorage.setItem('menuAberto', 'true')
+      } else {
+          abrir = 1;
+          fundoOpaco.style.display = 'none'
+          // Salve o estado no localStorage
+          localStorage.setItem('menuAberto', 'false')
+          console.log('fechado')
       }
   }
--- script/homeScript.js
+++ script/homeScript.js
@@ -1,4 +1,18 @@
-function carregar(){
+function carregar() {
     var video = document.getElementById("fundoDivPrincipal")
     video.playbackRate = 0.7;
+}
+
+function ibi() {
+    var texto = document.getElementById("easterEgg");
+    var n1 = 0;
+
+    while (n1 !== 3) {
+        n1++;
+    }
+
+    if (n1 === 3) {
+        texto.innerHTML = "IBIIIII";
+    }
+
 }
